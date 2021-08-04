355b63c13443334b617f20a165e6bd120a499d06
Kapish Pandoh <73287168+KapishPandoh@users.noreply.github.com>
2021-08-04 07:33:06 +0530

    Add files via upload

---
+++ Introduction to Scala/VariableDeclarartion.scala
@@ -0,0 +1,23 @@
+
+
+object VariableDeclarartion 
+{
+   def main(args :Array[String])
+   {
+     var myVar1:Int = 10;
+     var myVar2 = 20;
+     
+     println(myVar1)
+     println(myVar2)
+     
+     val myVal1:String = "Welcome to Scala";
+     val myVal2 = "Scala For DataScience";
+     
+     println(myVal1)
+     println(myVal2)
+     
+     // if we want to print it in same line use ;
+     //println(myVar1); println(myVar2); println(myVal1);   println(myVal2);b 
+   }
+  
+}
